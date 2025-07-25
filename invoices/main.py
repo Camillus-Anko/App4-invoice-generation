c2c6685fbcd0cdc76e89c779b3e675a85f9fbe51
Camillus Anko <ankocamillus@gmail.com>
2025-07-25 12:02:11 +0000

    used autopep8

--- invoices/main.py
+++ invoices/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import glob 
+import glob
 from fpdf import FPDF
 from pathlib import Path
 
@@ -9,16 +9,13 @@ for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="p", unit="mm", format="A4")
     pdf.add_page()
-    
-    filename = Path(filepath).stem #stem is a method that returns the file name
+
+    # stem is a method that returns the file name
+    filename = Path(filepath).stem
     invoice_nr = filename.split("-")[0]
     pdf.set_font(family="Times", size=16, style="B")
 
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
     pdf.output(f"PDFs/{filename}.pdf")
 
-
-
-
     # print(df)
-
