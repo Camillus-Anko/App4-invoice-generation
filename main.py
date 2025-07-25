c188548fe69e7c77649f23dd7dbc8eaabd6e0773
Camillus Anko <ankocamillus@gmail.com>
2025-07-25 15:05:38 +0000

    Day 26: Add a table

--- main.py
+++ main.py
@@ -17,11 +17,35 @@ for filepath in filepaths:
     invoice_nr, date = filename.split("-")  # Unpack the values
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)  # ln - breakline
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
-    pdf.output(f"PDFs/{filename}.pdf")
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
-    # print(df)
+    # Add Header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+
+pdf.output(f"PDFs/{filename}.pdf")
+
+# print(df)
